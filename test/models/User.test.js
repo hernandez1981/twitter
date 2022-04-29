6207522b5e7728b149a59497c05232a69684d1a9
hernandez1981 <kathya_lucia@hotmail.com>
2022-04-29 16:25:21 -0600

    Modificando el código dateCreated y lastUpdated a new Date() en prueba y clase User

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -3,12 +3,12 @@ const User = require('./../../app/models/user');
 describe("Unit Tests for User class", () => {
     
     test('Create an User object', () => { 
-        const user = new User(1, "carlogilmar", "Carlo", "Bio", "dateCreated", "lastUpdated")      
+        const user = new User(1, "carlogilmar", "Carlo", "Bio")      
         expect(user.id).toBe(1);
         expect(user.username).toBe("carlogilmar");
         expect(user.name).toBe("Carlo");
         expect(user.bio).toBe("Bio");
-        expect(user.dateCreated).toBe("dateCreated");//Verifica que el valor no sea undefine
-        expect(user.lastUpdated).toBe("lastUpdated");      
+        expect(user.dateCreated).not.toBeUndefined();
+        expect(user.lastUpdated).not.toBeUndefined();      
     });  
 })
