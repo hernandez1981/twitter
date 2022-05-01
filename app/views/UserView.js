c86703d27e94382de4a8d1f7ee13443e8e2dc217
hernandez1981 <kathya_lucia@hotmail.com>
2022-05-01 17:57:57 -0600

    Agregando carpeta views en app y test junto a clase y prueba UserView

---
+++ app/views/UserView.js
@@ -0,0 +1,41 @@
+const User = require('./../../app/models/User');
+
+class UserView {
+    constructor(){
+
+    }
+
+    static createUser(payload) {        
+        if (payload === null) {
+            return {
+                error : 'payload no existe'
+            }
+        }
+
+        // if (  payload.username === null || payload.name === null ||
+        //     payload.id === null ) {
+        //     return {
+        //         error : 'necesitan tener un valor válido'
+        //     }
+        // }
+
+        // if (  payload.username === undefined || payload.name === undefined  ||
+        //     payload.id === undefined  ) {
+        //     return {
+        //         error : 'necesitan tener un valor válido'
+        //     }
+        // }
+
+        // let user = new User();
+        // user.name = payload.name;
+        // user.username = payload.username;
+        // user.id = payload.id;
+        // return user;
+
+        // return new User(payload.id, payload.username, payload.name);
+
+    }
+
+}
+
+module.exports = UserView
---
+++ test/views/UserView.test.js
@@ -0,0 +1,32 @@
+const UserView = require('./../../app/views/UserView');
+
+describe("Tests for UserView", () => {
+
+    test("1. Return an error object when try to create a new user with an null payload", () => {
+        const payload = null;
+        const result = UserView.createUser(payload);
+        // https://jestjs.io/docs/using-matchers#strings
+        expect(result.error).toMatch(/payload no existe/);
+    });
+
+    // test("2. Return an error object when try to create a new user with a payload with invalid properties", () => {
+    //     const payload = { username: null, name:'Katy', id:'1x' };
+    //     const result = UserView.createUser(payload);        
+    //     expect(result.error).toMatch(/necesitan tener un valor válido/);
+    // });
+
+    // test("3. Return an error object when try to create a new user with a payload with missing properties", () => {
+    //     const payload = { username: "KatyHernandez" };
+    //     const result = UserView.createUser(payload);        
+    //     expect(result.error).toMatch(/necesitan tener un valor válido/);
+    // });
+
+    // test("4. Create a user by a given valid payload ", () => {
+    //     const payload = { username: "KatyHernandez", id:1, name : 'Katy Hernandez' };
+    //     const result = UserView.createUser(payload);        
+    //     expect(result.name).toBe("Katy Hernandez");
+    //     expect(result.username).toBe("KatyHernandez");
+    //     expect(result.id).toBe(1);
+    // });
+
+})
